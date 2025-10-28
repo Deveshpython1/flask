fb89f7783ef54b9da1ff5ea5563279acd6374bfd
Deveshpython1 <160889275+Deveshpython1@users.noreply.github.com>
2025-10-28 21:56:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,3 @@ app = Flask(__name__)
 def download_file():
     return jsonify({"name":"devesh pandey bhaiya"})
 
-if __name__ == '__main__':
-    # Accessible on local network
-    app.run(host='0.0.0.0', port=1234, debug=True)

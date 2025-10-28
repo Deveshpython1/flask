59692a44c530cdc8be6ecccfaa89768274befe6b
Deveshpython1 <160889275+Deveshpython1@users.noreply.github.com>
2025-10-28 22:02:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from flask import Flask, send_file,jsonify
 
 app = Flask(__name__)
 
-@app.route('/download', methods=['GET'])
+@app.route('/download')
 def download_file():
     return jsonify({"name":"devesh pandey bhaiya"})
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("PORT", default=8080))
 

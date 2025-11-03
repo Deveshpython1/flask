2db00ba2b9396a692139b2d29100d08644708cf3
Deveshpython1 <160889275+Deveshpython1@users.noreply.github.com>
2025-11-03 13:15:59 +0530

    Add environment variable support for Flask app

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask,render_template
-
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template("Bvin.html")
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=8080))
 

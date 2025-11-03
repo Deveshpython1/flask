d9c39199976068075886eb70c3ad237e20f3bffb
Deveshpython1 <160889275+Deveshpython1@users.noreply.github.com>
2025-11-03 22:20:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask,render_template
+from flask import Flask,render_template,send_file
 import os
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template("Bvin.html")
+@app.route("/dowmload")
+def hell():
+    return send_file("pinggy")
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=8080))
 
